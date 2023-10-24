5144d1a09859f4bc4a5d15f462693b5fd512de63
Mahshid <baharimahshid@gmail.com>
2023-10-24 23:46:43 +0200

    change the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request,'/home/dci-student/Desktop/excersises/django-home-project/djangogirls/blog/templates/blog/post_list.html',{})
